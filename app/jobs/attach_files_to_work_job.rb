3380aa935d600de95046efd8375017c9c7b854c8
Julie Allinson <julie.allinson@london.ac.uk>
2018-06-06 09:27:28 +0100

    Version 2 of the ordered members code, make it swappable

--- app/jobs/attach_files_to_work_job.rb
+++ app/jobs/attach_files_to_work_job.rb
@@ -1,20 +1,23 @@
 # Converts UploadedFiles into FileSets and attaches them to works.
-
 class AttachFilesToWorkJob < Hyrax::ApplicationJob
   queue_as Hyrax.config.ingest_queue_name
-  attr_reader :ordered_members, :uploaded_files
 
   # @param [ActiveFedora::Base] work - the work object
   # @param [Array<Hyrax::UploadedFile>] uploaded_files - an array of files to attach
   def perform(work, uploaded_files, **work_attributes)
-    @uploaded_files = uploaded_files
-    validate_files!
+    validate_files!(uploaded_files)
     depositor = proxy_or_depositor(work)
     user = User.find_by_user_key(depositor)
+    work_permissions = work.permissions.map(&:to_hash)
     metadata = visibility_attributes(work_attributes)
-    @ordered_members = work.ordered_members.to_a # Build array of ordered members
-    process_uploaded_files(user, metadata, work)
-    add_ordered_members(user, work)
+    uploaded_files.each do |uploaded_file|
+      actor = Hyrax::Actors::FileSetActor.new(FileSet.create, user)
+      actor.file_set.permissions_attributes = work_permissions
+      actor.create_metadata(metadata)
+      actor.create_content(uploaded_file)
+      actor.attach_to_work(work)
+      uploaded_file.update(file_set_uri: actor.file_set.uri)
+    end
   end
 
   private
@@ -27,7 +30,7 @@ class AttachFilesToWorkJob < Hyrax::ApplicationJob
                        :visibility_after_embargo)
     end
 
-    def validate_files!
+    def validate_files!(uploaded_files)
       uploaded_files.each do |uploaded_file|
         next if uploaded_file.is_a? Hyrax::UploadedFile
         raise ArgumentError, "Hyrax::UploadedFile required, but #{uploaded_file.class} received: #{uploaded_file.inspect}"
@@ -39,25 +42,5 @@ class AttachFilesToWorkJob < Hyrax::ApplicationJob
     # that the proxy was depositing on behalf of. See tickets #2764, #2902.
     def proxy_or_depositor(work)
       work.on_behalf_of.blank? ? work.depositor : work.on_behalf_of
-    end
-
-    # Add all file_sets as ordered_members in a single action
-    def add_ordered_members(user, work)
-      actor = Hyrax::Actors::OrderedMembersActor.new(ordered_members)
-      actor.attach_to_work(work)
-      actor.run_callback(user)
-    end
-
-    def process_uploaded_files(user, metadata, work)
-      work_permissions = work.permissions.map(&:to_hash)
-      uploaded_files.each do |uploaded_file|
-        actor = Hyrax::Actors::FileSetActor.new(FileSet.create, user)
-        actor.create_metadata(metadata)
-        actor.create_content(uploaded_file)
-        actor.attach_to_work(work)
-        actor.file_set.permissions_attributes = work_permissions
-        ordered_members << actor.file_set
-        uploaded_file.update(file_set_uri: actor.file_set.uri)
-      end
     end
 end
